47953c22cfd28114621ac9a55978c9b0684ce581
Srishti Nath <srishti.nath@gmail.com>
2020-04-01 04:25:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,17 @@
 class Dog
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @name = dogs_name
   end
 
   def name
-    @this_dogs_name
+    @name
   end
 
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+
+  def breed
+    @breed
+  end
 end
